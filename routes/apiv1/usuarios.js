2942ec94ebf8e22ae4ea13eab58e20d40202057d
Fabio Gómez <gomez.fabio@gmail.com>
2017-05-17 20:22:22 +0200

    Hash a la clave cuando guardamos el usuario

--- routes/apiv1/usuarios.js
+++ routes/apiv1/usuarios.js
@@ -4,9 +4,14 @@ const express  = require('express');
 const router   = express.Router();
 const mongoose = require('mongoose');
 const Usuario  = mongoose.model('Usuario');
+const crypto = require('crypto');
+const key = 'clavesupersecreta1234YMAYUSCULASyminusculas';
+let hash = crypto.createHmac('sha256', key);
 
 router.post('/', (req, res, next)=>{
    const usuario = new Usuario(req.body);
+    hash.update(usuario.clave)
+    usuario.clave = hash.digest('hex')
     usuario.save((err, usuarioGuardado) =>{
     if (err) {
         next(err);
